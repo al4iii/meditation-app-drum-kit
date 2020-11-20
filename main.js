6e08853e1ec3db8cf18b2018bc3090b1e1afd410
Aliaksandr Danilin <al4icrux@gmail.com>
2020-11-20 11:34:34 +0300

    fix: implement correct

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  e.target.classList.remove('playing');
-};
+
+const keys = document.querySelectorAll('.key');
+document.addEventListener('keydown', playSound);
+document.addEventListener('click', playSound);
 
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  if (!audio) return;
-
-  key.classList.add('playing');
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;    
   audio.currentTime = 0;
   audio.play();
+  key.classList.add('playing');
 };
 
-const keys = Array.from(document.querySelectorAll('.key'));
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+};
+
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
