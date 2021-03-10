0c5664d97a2a441ace638576e7745d9af2bc1aed
Ali Da <al4icrux@gmail.com>
2021-03-10 23:17:19 +0300

    refactor: apply prettier;

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let keys = document.querySelectorAll('.key');
+const keys = document.querySelectorAll('.key');
 
-let playSound = (e) => {
+const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);      
   audio.currentTime = 0;
